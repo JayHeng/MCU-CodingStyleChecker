851de6d9d2a4a599667294c1142052eba367ae77
Jay Heng <jie.heng@nxp.com>
2020-03-15 16:25:21 +0800

    [Checker Tool] Can check general comment
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

--- checker/src/main.py
+++ checker/src/main.py
@@ -7,14 +7,27 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QIcon
 from checkerWin import *
 
+kFileType_Source = u".c"
+kFileType_Header = u".h"
+
 class checkerMain(QMainWindow, Ui_MainWindow):
 
     def __init__(self, parent=None):
         super(checkerMain, self).__init__(parent)
         self.setupUi(self)
         self._registerCallbacks()
+        self._initSegmentMagic()
         self.progressBar.reset()
         self.fileFolderName = None
+
+    def _initSegmentMagic(self):
+        self.segmentMagicStart = u"/*******************************************************************************"
+        self.definitionMagic   = u" * Definitions"
+        self.variableMagic     = u" * Variables"
+        self.prototypeMagic    = u" * Prototypes"
+        self.codeMagic         = u" * Code"
+        self.apiMagic          = u" * API"
+        self.segmentMagicEnd   = u" ******************************************************************************/"
 
     def _registerCallbacks(self):
         self.pushButton_browseFileFolder.clicked.connect(self.callbackBrowseFileFolder)
@@ -28,8 +41,8 @@ class checkerMain(QMainWindow, Ui_MainWindow):
         self.textEdit_inputFileFolder.setPlainText(self.fileFolderName)
 
     def _fileCheckSeparator(self, filename):
-        self.textEdit_log.append(u"------------------------------------------------------------------------------")
-        self.textEdit_log.append(u"Start to check  " + filename)
+        self.textEdit_log.append(u">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>----------File----------<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+        self.textEdit_log.append(u"Start to check  " + filename + "\n")
 
     def _isUtf8AsciiFile(self, filename):
         self._fileCheckSeparator(filename)
@@ -37,28 +50,49 @@ class checkerMain(QMainWindow, Ui_MainWindow):
         fileDat = fileObj.read()
         # https://chardet.readthedocs.io/en/latest/supported-encodings.html
         fileProperties = chardet.detect(fileDat)
+        fileObj.close()
         if fileProperties.get('confidence') == 1.0 and \
            (fileProperties.get('encoding') != 'utf-8' or \
             fileProperties.get('encoding') != 'ascii'):
             return True
         else:
-            self.textEdit_log.append(u"【ERROR】: Cannot support Non-'UTF-8'/'ASCII'(100%) encoded file ")
+            self.textEdit_log.append(u"【ERROR】: Cannot support Non-'UTF-8'/'ASCII'(100%) encoded file \n")
             return False
 
-    def _doCheckSourceFile(self, sourceFilename):
-        pass
+    def _checkSegment(self, content, segmentMagic):
+        segment = self.segmentMagicStart + u"\n" + segmentMagic + u"\n" + self.segmentMagicEnd
+        if content.find(segment) == -1:
+            self.textEdit_log.append(u"【ERROR】: Below general comment is missed in the file")
+            self.textEdit_log.append(segment)
+
+    def _checkSegments(self, filename, fileType):
+        with open(filename, mode="r", encoding="utf-8") as fileObj:
+            content = fileObj.read()
+            self._checkSegment(content, self.definitionMagic)
+            if fileType == kFileType_Header:
+                self._checkSegment(content, self.apiMagic)
+            elif fileType == kFileType_Source:
+                self._checkSegment(content, self.variableMagic)
+                self._checkSegment(content, self.prototypeMagic)
+                self._checkSegment(content, self.codeMagic)
+            else:
+                pass
+            fileObj.close()
 
     def _doCheckHeaderFile(self, headerFilename):
-        pass
+        self._checkSegments(headerFilename, kFileType_Header)
+
+    def _doCheckSourceFile(self, sourceFilename):
+        self._checkSegments(sourceFilename, kFileType_Source)
 
     def _detectFileType(self, filename):
         if os.path.isfile(filename):
             filetype = os.path.splitext(filename)[1]
-            if filetype == '.h':
+            if filetype == kFileType_Header:
                 if not self._isUtf8AsciiFile(filename):
                     return
                 self._doCheckHeaderFile(filename)
-            elif filetype == '.c':
+            elif filetype == kFileType_Source:
                 if not self._isUtf8AsciiFile(filename):
                     return
                 self._doCheckSourceFile(filename)
@@ -78,7 +112,7 @@ class checkerMain(QMainWindow, Ui_MainWindow):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main_win = checkerMain()
-    main_win.setWindowTitle(u"MCUXpresso SDK Coding Style Checker v1.0.0")
+    main_win.setWindowTitle(u"MCUXpresso SDK Coding Style Checker v0.1")
     main_win.setWindowIcon(QIcon(u"../img/MCUX-SDK-CodingStyleChecker.ico"))
     main_win.show()
     sys.exit(app.exec_())
