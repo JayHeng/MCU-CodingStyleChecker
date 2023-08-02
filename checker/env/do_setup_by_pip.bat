ada85e37d3188212ec1f8038d4010499e40197c0
Jay Heng <jie.heng@nxp.com>
2023-08-02 11:14:52 +0800

    [Checker Tool] Re-test with Python3.7+PyQt5.15
    - It worked well
    
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

--- checker/env/do_setup_by_pip.bat
+++ checker/env/do_setup_by_pip.bat
@@ -1,5 +1,5 @@
-pip.exe install PyQt5==5.11.3 
-pip.exe install pyqt5-tools==5.11.3.1.4
-pip.exe install chardet==3.0.4
+pip.exe install PyQt5==5.15.1 
+pip.exe install pyqt5-tools==5.15.1.3
+pip.exe install chardet==4.0.0
 
 pip.exe install PyInstaller
